6869db33759f4e32f7cfa1af49cb69a17da2cac4
Veer <Veerbhojwani.27@gmail.com>
2026-03-14 20:29:42 -0500

    Initial Commit

---
+++ DateKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateKeeper
+//
+//  Created by Veer Bhojwani on 3/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateKeeper/DateKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateKeeperApp.swift
+//  DateKeeper
+//
+//  Created by Veer Bhojwani on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct DateKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
